bb5b9fe7aa45afd979a54fb8e0269d0d38e2355b
Mohit Pandey <86375666+mohitpandey95@users.noreply.github.com>
2025-09-01 18:23:25 +0530

    Create petrol-price-calculator.c

---
+++ Basic Programs/petrol-price-calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int petrol;
+ printf("Enter liters of petrol: ");
+ scanf("%d", &petrol);
+
+ int ppl = 103;
+ int totalcost = petrol * ppl;
+
+  printf("Total cost: %d IndianRupee\n", totalcost);
+
+
+
+return 0;
+}
